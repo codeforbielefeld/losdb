39f481379cde214f76ae701123ba8707923e69b7
Marcel Otto <marcelotto@gmx.de>
2021-05-06 00:06:41 +0200

    Add ChildrenPerHousehold dataset conversion

--- lib/losdb/dataset/household.ex
+++ lib/losdb/dataset/household.ex
@@ -7,17 +7,22 @@ defmodule LOSDB.Dataset.Household do
   import LOSDB.Dataset
 
   @datasource "data/haushalte/haushalte2009bis2020.csv"
-  @dataset_name "haushalte_anzahl_personen"
+  @anzahl_personen_dataset_name :haushalte_anzahl_personen
+  @anzahl_kinder_dataset_name :haushalte_anzahl_kinder
 
-  def extract_dataset(opts \\ []) do
-    extract_dataset(
-      @datasource,
-      Keyword.put_new(opts, :dataset_name, @dataset_name)
-    )
+  def extract_datasets(opts \\ []) do
+    extract_datasets(@datasource, opts)
   end
 
-  def extract_dataset(input_file, opts) do
+  def extract_datasets(input_file, opts) do
     records = raw_csv_records(input_file)
+
+    extract_dataset(@anzahl_personen_dataset_name, records, opts)
+    extract_dataset(@anzahl_kinder_dataset_name, records, opts)
+  end
+
+  def extract_dataset(dataset_name, records, opts) do
+    opts = Keyword.put(opts, :dataset_name, to_string(dataset_name))
 
     cube_dataset =
       opts
@@ -38,16 +43,14 @@ defmodule LOSDB.Dataset.Household do
 
     graph =
       Enum.reduce(records, metadata, fn record, graph ->
-        RDF.Graph.add(graph, cube_observation(record, cube_dataset.subject))
+        RDF.Graph.add(graph, cube_observation(dataset_name, record, cube_dataset.subject))
       end)
 
-    case Keyword.pop(opts, :write, false) do
-      {false, opts} -> graph
-      {true, opts} -> write(graph, opts)
-    end
+    write(graph, opts)
   end
 
   def cube_observation(
+        @anzahl_personen_dataset_name,
         [
           _stichtag,
           jahr,
@@ -69,7 +72,7 @@ defmodule LOSDB.Dataset.Household do
         ],
         dataset
       ) do
-    base_observation = [
+    [
       RDF.bnode()
       |> RDF.type(Cube.Observation)
       |> Cube.dataSet(dataset)
@@ -91,6 +94,61 @@ defmodule LOSDB.Dataset.Household do
       |> Vocab.place(StatBezirk.iri("0" <> stat_id))
       |> Vocab.peoplePerHousehold(Vocab.ThreeOrMorePersonHousehold)
       |> Vocab.numberOfHouseholds(XSD.integer(dreiOderMehrPersonenHaushalte))
+    ]
+  end
+
+  def cube_observation(
+        @anzahl_kinder_dataset_name,
+        [
+          _stichtag,
+          jahr,
+          stat_id,
+          _stat_name,
+          _hh_insgesamt,
+          _einPersonenHaushalte,
+          _zweiPersonenHaushalte,
+          _dreiOderMehrPersonenHaushalte,
+          hh_keineKinder,
+          _haushalt_mitKindern_insgesamt,
+          hh_ein_Kind,
+          hh_zwei_Kinder,
+          hh_drei_oder_mehrKindern,
+          _hh_EhePaareohneKinder_evtl_weiterePersonen,
+          _hh_EhePaaremitKindern_evtl_weiterePersonen,
+          _hh_Alleinerziehende_ohne_weiterePersonen,
+          _hh_SonstigeMehrpersonenhaushalte
+        ],
+        dataset
+      ) do
+    [
+      RDF.bnode()
+      |> RDF.type(Cube.Observation)
+      |> Cube.dataSet(dataset)
+      |> Vocab.refPeriod(RDF.literal(jahr, datatype: RDF.NS.XSD.gYear()))
+      |> Vocab.place(StatBezirk.iri("0" <> stat_id))
+      |> Vocab.childrenPerHousehold(Vocab.NoChildHousehold)
+      |> Vocab.numberOfHouseholds(XSD.integer(hh_keineKinder)),
+      RDF.bnode()
+      |> RDF.type(Cube.Observation)
+      |> Cube.dataSet(dataset)
+      |> Vocab.refPeriod(RDF.literal(jahr, datatype: RDF.NS.XSD.gYear()))
+      |> Vocab.place(StatBezirk.iri("0" <> stat_id))
+      |> Vocab.childrenPerHousehold(Vocab.OneChildHousehold)
+      |> Vocab.numberOfHouseholds(XSD.integer(hh_ein_Kind)),
+      RDF.bnode()
+      |> RDF.type(Cube.Observation)
+      |> Cube.dataSet(dataset)
+      |> Vocab.refPeriod(RDF.literal(jahr, datatype: RDF.NS.XSD.gYear()))
+      |> Vocab.place(StatBezirk.iri("0" <> stat_id))
+      |> Vocab.childrenPerHousehold(Vocab.TwoChildrenHousehold)
+      |> Vocab.numberOfHouseholds(XSD.integer(hh_zwei_Kinder)),
+      RDF.bnode()
+      |> RDF.type(Cube.Observation)
+      |> Cube.dataSet(dataset)
+      |> Vocab.refPeriod(RDF.literal(jahr, datatype: RDF.NS.XSD.gYear()))
+      |> Vocab.place(StatBezirk.iri("0" <> stat_id))
+      |> Vocab.childrenPerHousehold(Vocab.ThreeOrMoreChildrenHousehold)
+      |> Vocab.numberOfHouseholds(XSD.integer(hh_drei_oder_mehrKindern))
     ]
   end
 end
