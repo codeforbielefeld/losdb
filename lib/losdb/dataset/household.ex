9cbac06bd20ac092f08343027bcc30e649a939c2
Marcel Otto <marcelotto@gmx.de>
2021-05-12 21:38:54 +0200

    Add ChildrenPerHousehold dataset conversion

--- lib/losdb/dataset/household.ex
+++ lib/losdb/dataset/household.ex
@@ -8,6 +8,7 @@ defmodule LOSDB.Dataset.Household do
   @datasource "data/haushalte/haushalte2009bis2020.csv"
   @anzahl_personen_dataset_name :haushalte_anzahl_personen
   @anzahl_kinder_dataset_name :haushalte_anzahl_kinder
+  @wohngemeinschaft_data_name :haushalte_wohngemeinschaften
 
   def extract_datasets(opts \\ []) do
     extract_datasets(@datasource, opts)
@@ -18,6 +19,7 @@ defmodule LOSDB.Dataset.Household do
 
     extract_dataset(@anzahl_personen_dataset_name, records, opts)
     extract_dataset(@anzahl_kinder_dataset_name, records, opts)
+    extract_dataset(@wohngemeinschaft_data_name, records, opts)
   end
 
   def extract_dataset(dataset_name, records, opts) do
@@ -148,6 +150,61 @@ defmodule LOSDB.Dataset.Household do
       |> Vocab.place(StatBezirk.iri("0" <> stat_id))
       |> Vocab.childrenPerHousehold(Vocab.ThreeOrMoreChildrenHousehold)
       |> Vocab.numberOfHouseholds(XSD.integer(hh_drei_oder_mehrKindern))
+    ]
+  end
+
+  def cube_observation(
+        @wohngemeinschaft_data_name,
+        [
+          _stichtag,
+          jahr,
+          stat_id,
+          _stat_name,
+          _hh_insgesamt,
+          _einPersonenHaushalte,
+          _zweiPersonenHaushalte,
+          _dreiOderMehrPersonenHaushalte,
+          _hh_keineKinder,
+          _haushalt_mitKindern_insgesamt,
+          _hh_ein_Kind,
+          _hh_zwei_Kinder,
+          _hh_drei_oder_mehrKindern,
+          hh_EhePaareohneKinder_evtl_weiterePersonen,
+          hh_EhePaaremitKindern_evtl_weiterePersonen,
+          hh_Alleinerziehende_ohne_weiterePersonen,
+          hh_SonstigeMehrpersonenhaushalte
+        ],
+        dataset
+      ) do
+    [
+      RDF.bnode()
+      |> RDF.type(Cube.Observation)
+      |> Cube.dataSet(dataset)
+      |> Vocab.refPeriod(RDF.literal(jahr, datatype: RDF.NS.XSD.gYear()))
+      |> Vocab.place(StatBezirk.iri("0" <> stat_id))
+      |> Vocab.householdCommunity(Vocab.CoupleWithoutKidsHousehold)
+      |> Vocab.numberOfHouseholds(XSD.integer(hh_EhePaareohneKinder_evtl_weiterePersonen)),
+      RDF.bnode()
+      |> RDF.type(Cube.Observation)
+      |> Cube.dataSet(dataset)
+      |> Vocab.refPeriod(RDF.literal(jahr, datatype: RDF.NS.XSD.gYear()))
+      |> Vocab.place(StatBezirk.iri("0" <> stat_id))
+      |> Vocab.householdCommunity(Vocab.CoupleWithKidsHousehold)
+      |> Vocab.numberOfHouseholds(XSD.integer(hh_EhePaaremitKindern_evtl_weiterePersonen)),
+      RDF.bnode()
+      |> RDF.type(Cube.Observation)
+      |> Cube.dataSet(dataset)
+      |> Vocab.refPeriod(RDF.literal(jahr, datatype: RDF.NS.XSD.gYear()))
+      |> Vocab.place(StatBezirk.iri("0" <> stat_id))
+      |> Vocab.householdCommunity(Vocab.SingleParentHousehold)
+      |> Vocab.numberOfHouseholds(XSD.integer(hh_Alleinerziehende_ohne_weiterePersonen)),
+      RDF.bnode()
+      |> RDF.type(Cube.Observation)
+      |> Cube.dataSet(dataset)
+      |> Vocab.refPeriod(RDF.literal(jahr, datatype: RDF.NS.XSD.gYear()))
+      |> Vocab.place(StatBezirk.iri("0" <> stat_id))
+      |> Vocab.householdCommunity(Vocab.OtherMultiPersonHousehold)
+      |> Vocab.numberOfHouseholds(XSD.integer(hh_SonstigeMehrpersonenhaushalte))
     ]
   end
 end
